c7a1f9d94eca83509c461a615e34c9d9cf9d56c6
subbu8repo <137267398+subbu8repo@users.noreply.github.com>
2024-04-30 19:03:12 +0530

    Create Istio.sh

---
+++ Istio.sh
@@ -0,0 +1,7 @@
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-1.21.2
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+kubectl apply -f samples/addons/kiali.yaml
+kubectl apply -f istio-1.20.3/samples/addons/prometheus.yaml
+kubectl label namespace default istio-injection=enabled
